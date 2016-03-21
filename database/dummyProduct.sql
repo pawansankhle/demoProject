36addce4623a1f9e68b12e8f48591b8786843941
pawansankhle <pawansankhle@gmail.com>
2016-03-21 11:32:30 +0530

    sql changes

--- database/dummyProduct.sql
+++ database/dummyProduct.sql
@@ -1,16 +1,21 @@
-insert into product values(2,now(),now(),'fish food',500,5,'this product is highly valueable for high breed fish',10,1,4,2,1);
-
-
-
+insert into product values(1,now(),now(),'fish food',500,5,'this product is highly valueable for high breed fish',10,1,4,2,1);
 insert into product values(2,now(),now(),'dogfood',500,5,'sdlkjsd',10,1,1,1,1);
 insert into product values(3,now(),now(),'puppy food',300,5,'slkjsl',10,1,1,1,1);
 insert into product values(4,now(),now(),'dog food',600,5,'lksj',10,1,1,1,1);
 insert into product values(5,now(),now(),'puppy food',700,5,'sldkjf',10,1,1,1,1);
 insert into product values(6,now(),now(),'puppy food',400,5,'sldkfjs',10,1,1,1,1);
+insert into product values(7,now(),now(),'puppy food',800,5,'sldkfjs',10,1,1,1,1);
 
 
-insert into file_upload values(9,'','product thumbnil');
-insert into file_upload values(10,'','product thumbnil');
-insert into file_upload values(11,'','product thumbnil');
-insert into file_upload values(12,'','product thumbnil');
-insert into file_upload values(13,'','product thumbnil');
+insert into file_upload values(9,'images/dog_food.jpg','product thumbnil');
+insert into file_upload values(10,'images/belt.jpg','product thumbnil');
+insert into file_upload values(11,'images/drool2.jpg','product thumbnil');
+insert into file_upload values(12,'images/drool3.jpg','product thumbnil');
+insert into file_upload values(13,'images/images.jpg','product thumbnil');
+
+
+insert into product_file_upload values(3,9);
+insert into product_file_upload values(4,10);
+insert into product_file_upload values(5,11);
+insert into product_file_upload values(6,12);
+insert into product_file_upload values(7,13);
